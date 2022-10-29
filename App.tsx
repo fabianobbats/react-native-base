ac21cc1f1d415af5547081771e9538d38c8f815e
Fabiano Batista <fabianobatista@outlook.com>
2022-10-29 09:07:03 -0300

    adicionado react navigation e useContext para autenticacao

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { StatusBar } from 'react-native';
 
-export default function App() {
+import { AuthProvider } from './src/contexts/auth';
+import Routes from './src/routes';
+
+
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+      <NavigationContainer>
+        <AuthProvider> 
+          <Routes />
+        </AuthProvider>
+      </NavigationContainer>
+      <StatusBar/>
+    </>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
